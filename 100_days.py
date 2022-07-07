c4be476778a47f71067876555ef4f50c13647f33
watermanbrandyn <watermanbrandyn@gmail.com>
2022-07-06 21:17:37 -0500

    100 Days of Code

---
+++ 100_days.py
@@ -0,0 +1,12 @@
+# From 100 Days of Code: The Complete Python Pro Bootcamp for 2022, with Dr. Angela Yu
+# Main project from each day
+
+# Day 1 project
+# Band name generator
+
+print("Welcome to the band name generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+band = city + " " + pet
+print(f"Your band name is {band}!")
+
