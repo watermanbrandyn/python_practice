87d11a9fe907525e0a5040b3c1070392572bdd33
watermanbrandyn <watermanbrandyn@gmail.com>
2022-07-07 15:10:08 -0500

    100 days

--- 100_days.py
+++ 100_days.py
@@ -10,3 +10,24 @@ pet = input("What is your pet's name?\n")
 band = city + " " + pet
 print(f"Your band name is {band}!")
 
+# Day 2 project
+# Tip calculator
+
+print("Welcome to the tip calculator!\n")
+
+bill = input("What was the total bill?\n")
+tip = input("What tip percentage would you like to leave?\n")
+party = input("How many people are in your party?\n")
+
+bill = float(bill)
+tip = int(tip)
+party = int(party)
+
+def tip_calculator(bill, tip, party):
+    per_person = (bill / party) * (1 + (tip / 100))
+    return per_person
+
+per_person_amount = tip_calculator(bill, tip, party)
+# To round to two decimal places and force presentation of both (even if 0 value)
+print(f"Each person should pay ${per_person_amount:.2f}.")
+    
