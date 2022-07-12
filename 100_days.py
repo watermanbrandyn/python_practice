7744fb7a4da78265a3abbc744b7e591a5d7c2e28
watermanbrandyn <watermanbrandyn@gmail.com>
2022-07-11 22:46:42 -0500

    100 days

--- 100_days.py
+++ 100_days.py
@@ -120,4 +120,50 @@ play_game()
 
 # Day 5 projects
 
+# Fizzbuzz
+
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("Fizzbuzz")
+    elif n % 3 == 0:
+        print('Fizz')
+    elif n % 5 == 0:
+        print('Buzz')
+    else:
+        print(n)
+
+# Password Generator 
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+password = ''
+
+for i in range(nr_letters):
+    password += random.choice(letters)
+for i in range(nr_symbols):
+    password += random.choice(symbols)
+for i in range(nr_numbers):
+    password += random.choice(numbers)
+
+print(f"Your new password is: {password}")
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+print(f"Your new randomised password is: {''.join(random.sample(password, len(password)))}")
+
+
+
+
 
